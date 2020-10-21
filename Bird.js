34fe549642c0d63d7cce55401bc460d1b852ae68
techhat18 <munjalakshat18@gmail.com>
2020-10-21 21:11:11 +0530

    stage6

--- Bird.js
+++ Bird.js
@@ -2,6 +2,8 @@ class Bird extends BaseClass {
   constructor(x,y){
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
+    this.smokeImage = loadImage("sprites/smoke.png");
+    this.trajectory = [];
   }
 
   display() {
@@ -9,5 +11,12 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
+    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+      var disco = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(disco);
+    }  
+    for(var dancer = 0; dancer < this.trajectory.length; dancer++){
+      image(this.smokeImage, this.trajectory[dancer][0], this.trajectory[dancer][1]);
+    }
   }
 }
